153162fe13b1cc175efa74e468118eab928e7afe
Africanfruit <slimbion@gmail.com>
2022-09-17 23:20:26 +0100

    Added HotJar analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import {Amplify} from "@aws-amplify/core";
 import awsConfigs from '../src/aws-exports';
 import * as Sentry from "@sentry/react";
 import {BrowserTracing} from "@sentry/tracing";
+import Script from "next/script";
 
 Amplify.configure({...awsConfigs, ssr: true});
 
@@ -27,6 +28,16 @@ function MyApp({Component, pageProps}) {
                       content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/>
                 <link rel="icon" href="/fittr_symbol_small.svg"/>
                 <title>Fittree | Your workouts everywhere</title>
+                <Script dangerouslySetInnerHTML={
+                    `(function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:3162126,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+                }/>
             </Head>
             <Provider store={store}>
                 <Component {...pageProps} />
