05417b89437632c51ddaec1365f97adfb05b043d
Africanfruit <slimbion@gmail.com>
2022-10-02 12:26:59 +0100

    temp 1

--- next.config.js
+++ next.config.js
@@ -43,11 +43,11 @@ const securityHeaders = [
 module.exports = {
     async headers() {
         return [
-            // {
-            //     // Apply these headers to all routes in your application.
-            //     source: '/',
-            //     headers: securityHeaders,
-            // },
+            {
+                // Apply these headers to all routes in your application.
+                source: '/',
+                headers: securityHeaders,
+            },
             // {
             //     // Apply these headers to all routes in your application.
             //     source: '/:path*',
