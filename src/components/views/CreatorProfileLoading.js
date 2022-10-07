e44d2ee36ee4ff650f7e3c4b4e6022d19f0b7fa4
Africanfruit <slimbion@gmail.com>
2022-10-07 20:37:23 +0100

    tunrned on CSP

--- src/components/views/CreatorProfileLoading.js
+++ src/components/views/CreatorProfileLoading.js
@@ -5,7 +5,9 @@ import FittrIconSmall from "../svg/fittr_symbol_small.svg";
 const CreatorProfileLoading = () => {
 
     return (
-        <div className="flex flex-col items-center fixed top-0 right-0 bottom-0 left-0 h-screen w-screen">
+
+        <div
+            className="flex flex-col items-center justify-center fixed top-0 right-0 bottom-0 left-0 h-screen w-screen">
             <FittrIconSmall/>
         </div>
     );
