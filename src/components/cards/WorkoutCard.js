297487a5d2f72af68fe0240eefea1bb5bf01f89d
Africanfruit <slimbion@gmail.com>
2022-10-07 17:03:47 +0100

    Fixed issue with image not loading

--- src/components/cards/WorkoutCard.js
+++ src/components/cards/WorkoutCard.js
@@ -5,7 +5,7 @@ const WorkoutCard = ({workout}) => {
     console.log(workout)
 
     return (
-        <div className={'relative bg-[url(https://' + workout.thumbnailUrl + ')] h-60 bg-no-repeat bg-cover bg-center rounded-lg p-2 flex flex-col justify-end text-white'}>
+        <div className={`relative bg-[url(https://${workout.thumbnailUrl})] h-60 bg-no-repeat bg-cover bg-center rounded-lg p-2 flex flex-col justify-end text-white`}>
             <div className="absolute top-0 right-0 m-2 py-0.5 px-2 rounded-md bg-primary text-xs font-medium">
                 <p>{Math.round(workout.duration / 60000)} mins</p>
             </div>
