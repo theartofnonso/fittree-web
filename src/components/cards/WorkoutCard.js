e88d407359a118f04b04965851dff7f0ac85c48e
Africanfruit <slimbion@gmail.com>
2022-10-08 17:09:36 +0100

    Fixed image issue

--- src/components/cards/WorkoutCard.js
+++ src/components/cards/WorkoutCard.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 const WorkoutCard = ({workout}) => {
 
-    console.log(workout.thumbnailUrl)
-
     return (
-        <div className={`relative bg-[url(https://d2ez6lox3k9lt0.cloudfront.net/public/Thumbnails/EyWL2QZSw7ci0Xu6UamE0IbH6920mMVaXWqaeni7awKMaVe4wb.jpg)] h-60 bg-no-repeat bg-cover bg-center rounded-lg p-2 flex flex-col justify-end text-white overflow-hidden hover:bg-transparentBlack`}>
+        <div className={`relative h-60 rounded-lg flex flex-col justify-end text-white overflow-hidden hover:bg-secondary`}>
+            <img src={"https://" + workout.thumbnailUrl} alt="Display profile" className="object-cover h-full w-full"/>
             <div className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b from-transparentBlack1 to-transparentBlack"/>
             <div className="absolute top-0 right-0 m-2 py-0.5 px-2 rounded-md bg-primary text-xs font-medium">
                 <p>{Math.round(workout.duration / 60000)} mins</p>
             </div>
-            <div className="absolute flex flex-col items-start">
+            <div className="absolute flex flex-col items-start pl-2 pb-2">
                 <p className="font-bold text-left">{workout.title}</p>
                 <p className="text-sm">{workout.intensityLevel}</p>
             </div>
--- src/components/cards/WorkoutCardBig.js
+++ src/components/cards/WorkoutCardBig.js
@@ -4,12 +4,13 @@ const WorkoutCardBig = ({workout}) => {
 
     return (
         <div
-            className={`relative bg-[url(https://${workout.thumbnailUrl})] h-96 bg-no-repeat bg-cover bg-center rounded-lg p-2 flex flex-col justify-end text-white overflow-hidden`}>
-            <div className="absolute top-0 right-0 bottom-0 left-0  bg-gradient-to-b from-transparentBlack1 to-transparentBlack"/>
+            className={`relative h-96 rounded-lg flex flex-col justify-end text-white overflow-hidden`}>
+            <img src={"https://" + workout.thumbnailUrl} alt="Display profile" className="object-cover h-full w-full"/>
+            <div className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b from-transparentBlack1 to-transparentBlack"/>
             <div className="absolute top-0 right-0 m-2 py-0.5 px-2 rounded-md bg-primary text-xs font-medium">
                 <p>{Math.round(workout.duration / 60000)} mins</p>
             </div>
-            <div className="absolute flex flex-col items-start">
+            <div className="absolute flex flex-col items-start pl-2 pb-2">
                 <p className="font-bold text-left">{workout.title}</p>
                 <p className="text-sm font-medium my-0.5">{workout.intensityLevel}</p>
                 <div className="flex flex-row text-sm overflow-x-scroll my-0.5">
