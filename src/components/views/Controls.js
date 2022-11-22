6862990b7ee7b5fa26b9361b74b4f7a21179375b
Africanfruit <slimbion@gmail.com>
2022-11-22 00:29:27 +0000

    Update Controls.js

--- src/components/views/Controls.js
+++ src/components/views/Controls.js
@@ -1,12 +1,15 @@
 /* eslint-disable */
 import React from "react";
+import PauseIcon from "../../assets/svg/pause-mini-line.svg";
 
 const Controls = (props) => {
 
     return (
-        <div
-            className="flex flex-row bg-primary justify-center w-48 rounded-md space-x-10 py-2 px-2 text-white font-semibold">
+        <div className="flex flex-row bg-primary justify-center w-48 rounded-md space-x-4 py-2 px-3 text-white font-semibold">
             <button type="button" className="" onClick={props.seekBackward}>Prev</button>
+                <button type="button" className="mx-2" onClick={props.pause}>
+                    <PauseIcon/>
+                </button>
             <button type="button" className="" onClick={props.seekForward}>Next</button>
         </div>
     );
