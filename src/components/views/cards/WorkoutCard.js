2a422c313d625b49dd87a0371c5c64b3ad19e336
Africanfruit <slimbion@gmail.com>
2022-11-24 22:49:23 +0000

    Update WorkoutCard.js

--- src/components/views/cards/WorkoutCard.js
+++ src/components/views/cards/WorkoutCard.js
@@ -6,7 +6,7 @@ const WorkoutCard = ({workout}) => {
     return (
         <div
             className="relative h-32 rounded-lg flex flex-col justify-end text-white overflow-hidden hover:bg-secondary cursor-pointer">
-            <img src={workout.thumbnailUrl ? "https://" + workout.thumbnailUrl : "/wallpaper.jpg"} alt="Display profile" className="object-cover h-full w-full"/>
+            <img src="/wallpaper.jpg" alt="Display profile" className="object-cover h-full w-full"/>
             <div
                 className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b from-transparentBlack1 to-transparentBlack hover:bg-transparentBlack1"/>
             <div className="absolute top-0 right-0 m-2 py-0.5 px-2 rounded-full bg-primary text-xs font-medium">
