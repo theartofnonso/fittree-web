0ece0579cd56df1a9d3b1b961de610b53ea7c366
Africanfruit <slimbion@gmail.com>
2022-11-27 12:43:36 +0000

    Fixed

--- src/components/views/cards/WorkoutCard.js
+++ src/components/views/cards/WorkoutCard.js
@@ -6,16 +6,13 @@ const WorkoutCard = ({workout}) => {
 
     return (
         <div
-            className="relative h-40 sm:h-52 rounded-lg flex flex-col justify-end text-white overflow-hidden hover:bg-darkSecondary cursor-pointer ">
-            <img src="/wallpaper.jpg" alt="Display profile" className="object-cover h-full w-full"/>
-            <div
-                className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b from-transparentBlack1 to-transparentBlack1 hover:bg-transparentBlack1"/>
+            className="relative h-52 rounded-lg flex flex-col justify-end text-white overflow-hidden hover:bg-black cursor-pointer">
+            <img src="/wallpaper.jpg" alt="Display profile" className="object-cover h-full w-full object-top"/>
             <div className="absolute top-0 right-0 m-2 py-0.5 px-2 rounded-full bg-primary text-xs font-bold">
                 <p>{workoutTagDisplay(false, workout)}</p>
             </div>
-            <div className={`absolute right-0 bottom-0 left-0 pl-3 pb-3 space-y-1 text-white font-bold`}>
-                <p className="text-left">{workout.title}</p>
-                <p className="text-sm">{workout.intensityLevel}</p>
+            <div className={`absolute right-0 bottom-0 left-0 pl-3 pb-3 space-y-1`}>
+                <p className="text-left  text-darkPrimary text-xl font-extrabold">{workout.title}</p>
 
                 <div className="flex flex-row font-bold">
                     <div
--- src/components/views/cards/WorkoutCardBig.js
+++ src/components/views/cards/WorkoutCardBig.js
@@ -10,22 +10,20 @@ const WorkoutCardBig = ({workout, showExtras}) => {
             duration={500}
             className="rounded-lg overflow-hidden relative rounded-lg text-white mb-3"
             height={showExtras ? "auto" : "20%"}>
-            <img src="/wallpaper.jpg" alt="Display profile" className="object-cover rounded-lg sm:h-52 sm:w-full"/>
-            <div
-                className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b from-transparentBlack1 to-transparentBlack1 rounded-lg"/>
+            <img src="/wallpaper.jpg" alt="Display profile" className="object-cover object-top rounded-lg sm:h-52 sm:w-full"/>
             <div className="absolute top-0 right-0 m-2 py-0.5 px-2 rounded-full bg-primary text-xs font-bold">
                 <p>{workoutTagDisplay(false, workout)}</p>
             </div>
-            <div className={`absolute right-0 bottom-0 left-0 pl-3 pb-3 space-y-1.0 font-bold`}>
-                <p className="text-left">{workout.title}</p>
-                <p className="text-sm">{workout.intensityLevel}</p>
+            <div className={`absolute right-0 bottom-0 left-0 pl-3 pb-3 space-y-1`}>
+                <p className="text-left  text-darkPrimary text-xl font-extrabold">{workout.title}</p>
+                <p className="text-sm font-bold">{workout.intensityLevel}</p>
 
-                <div className="flex flex-row mt-1.5">
+                <div className="flex flex-row font-bold">
                     <div
-                        className={`flex flex-row items-center px-2 py-0.5 w-fit bg-white text-primary ${workout.type === workoutsConstants.workoutType.CIRCUIT ? "rounded-l" : "rounded"} text-xs font-semibold`}>{workout.workoutExercises.length} exercises
+                        className={`flex flex-row items-center px-2 py-0.5 w-fit bg-white text-primary ${workout.type === workoutsConstants.workoutType.CIRCUIT ? "rounded-l" : "rounded"} text-xs`}>{workout.workoutExercises.length} exercises
                     </div>
                     {workout.type === workoutsConstants.workoutType.CIRCUIT ?
-                        <div className="flex flex-row items-center ml-0.5 px-2 py-0.5 w-fit bg-white text-primary rounded-r text-xs font-semibold">
+                        <div className="flex flex-row items-center ml-0.5 px-2 py-0.5 w-fit bg-white text-primary rounded-r text-xs">
                             {workout.rounds} Rounds
                         </div> : null}
                 </div>
