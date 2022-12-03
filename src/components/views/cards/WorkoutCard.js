37c36b46eb259dd39876767f3a7ccbdc7c103442
Africanfruit <slimbion@gmail.com>
2022-12-03 12:14:02 +0000

    fixed

--- src/components/views/cards/WorkoutCard.js
+++ src/components/views/cards/WorkoutCard.js
@@ -14,8 +14,8 @@ const WorkoutCard = ({workout}) => {
                     <p className="text-left text-white text-xl font-extrabold my-2">{workout.title}</p>
                     <p className="text-sm font-semibold">{workout.intensityLevel}</p>
                     <div className="flex flex-row font-medium space-x-1">
-                        <p className="text-white rounded-md text-sm">{workout.workoutExercises.length} exercises</p>
-                        {workout.type === workoutsConstants.workoutType.CIRCUIT && <p className="bg-white rounded px-2 text-dustBlack rounded-md text-sm">{workout.rounds} rnds</p>}
+                        <p className="text-white text-sm">{workout.workoutExercises.length} exercises</p>
+                        {workout.type === workoutsConstants.workoutType.CIRCUIT && <p className="bg-white rounded-full px-2 text-dustBlack rounded-md text-sm">{workout.rounds} rnds</p>}
                     </div>
                 </div>
             </div>
